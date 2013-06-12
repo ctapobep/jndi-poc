7ba402a17467b16ea6c633698ec779de4130ce4d
ctapobep <ctapobep@javatalks.ru>
2013-06-12 09:46:39 +0400

    added test for Name Not Found case

--- src/test/java/SimpleJndiTest.java
+++ src/test/java/SimpleJndiTest.java
@@ -3,6 +3,7 @@ import org.testng.annotations.Test;
 import javax.naming.InitialContext;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
 
 /** @author stanislav bashkirtsev */
 public class SimpleJndiTest {
@@ -10,5 +11,11 @@ public class SimpleJndiTest {
     public void testSimpleJndi() throws Exception {
         InitialContext context = new InitialContext();
         assertEquals(context.lookup("test.testobject"), "hello!");
+    }
+
+    @Test
+    public void testSimpleJndiDoesNotFollowSpecsAndReturnsNullInsteadOfThrowing() throws Exception {
+        InitialContext context = new InitialContext();
+        assertNull(context.lookup("test.does_not_exist"));
     }
 }
